0d8371fc8babc5c3938757392ce185c3c816710d
Sankar0805 <roalityzoro@gmail.com>
2025-10-17 11:21:01 +0530

    Update asgi.py

--- ChatProject/asgi.py
+++ ChatProject/asgi.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatProject.settings')
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 from ChatApp import routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatProject.settings')
 
 django_asgi_app = get_asgi_application()
 
