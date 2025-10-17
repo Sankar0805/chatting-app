093ba44fc8e0c6a37c001b8afc2413bfb3467bcf
Sankar0805 <roalityzoro@gmail.com>
2025-10-17 07:49:12 +0530

    Delete ChatApp directory

--- ChatApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Room)
-
-class MessageAdmin(admin.ModelAdmin):
-    list_display = ['room', 'sender', 'message']
-
-admin.site.register(Message, MessageAdmin)
